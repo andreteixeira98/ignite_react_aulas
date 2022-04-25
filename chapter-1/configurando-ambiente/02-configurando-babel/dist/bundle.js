9134b4e3db9607b1181ff5f4329dc9b10b417d1a
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-04-24 22:31:04 -0300

    add directory dist

---
+++ chapter-1/configurando-ambiente/02-configurando-babel/dist/bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var full_name = {
+  first: 'Andre',
+  last: 'Teixeira'
+};
+
+if ((full_name === null || full_name === void 0 ? void 0 : full_name.last) === 'Teixeira') {
+  console.log('true');
+}
