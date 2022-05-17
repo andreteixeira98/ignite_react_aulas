985fa04ab77653e877f05039ebb5d50fb589a6f4
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-05-17 15:03:02 -0300

    setup miragejs

--- chapter-2/dtmoney/src/index.tsx
+++ chapter-2/dtmoney/src/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createServer} from 'miragejs'
 import { App } from './App';
+
+
+
+createServer({
+  routes(){
+    this.namespace='/api';
+    this.get('/transactions',()=>{
+      return [
+        {
+          id:1,
+          title:'computer',
+          amount:5000,
+          type:'deposit',
+          category:'vendas',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
+
 root.render(
   <React.StrictMode>
     <App />
